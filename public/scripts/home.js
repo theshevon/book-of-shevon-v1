c8ed706e2e7b2303fca5763c31f496b3fa82c708
Shevon Mendis <shevonmendis98@gmail.com>
2019-03-03 21:40:04 +1100

    updates to blog, home + navbar fixes

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,9 +1,9 @@
-var gradients = ["linear-gradient(to top, #f77062 0%, #fe5196 100%)", 
-                 "linear-gradient(to top, #9be15d 0%, #00e3ae 100%)", 
-                 "linear-gradient(to top, #209cff 0%, #68e0cf 100%)"];
+// var gradients = ["linear-gradient(to top, #f77062 0%, #fe5196 100%)", 
+//                  "linear-gradient(to top, #9be15d 0%, #00e3ae 100%)", 
+//                  "linear-gradient(to top, #209cff 0%, #68e0cf 100%)"];
 
 window.onload = function(){
-    var n = Math.round(Math.random() * (gradients.length - 1));
-    $("#banner").css("background-image", gradients[n]);
+    // var n = Math.round(Math.random() * (gradients.length - 1));
+    // $("#banner").css("background-image", gradients[n]);
     $("#banner").children("h1").fadeIn(500);
 }
